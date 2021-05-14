a69ef0a7da2e1965697051e05d030421273c976f
wdu9 <71188956+wdu9@users.noreply.github.com>
2021-05-14 00:12:16 -0400

    Add files via upload

--- FBS-NK/reproduce.sh
+++ FBS-NK/reproduce.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # install the versioned required packages
-python3 -m pip install --quiet -r requirements.txt
+
+python3 -m pip install econ-ark == 0.11.0
+
+python -m pip install numpy == 1.20.2
+    
+python -m pip install matplotlib == 3.4.1
+  
+python -m pip install  scipy==1.6.2
+
+
 
 # navigate to code/ and execute the python file to create figures
 cd ./code/python
-jupyter nbconvert --to python FBS-NK-model.ipynb
+
+jupyter notebook FBS-NK-model.ipynb
 ipython Jacobian-SS.py
